4ffd4b0940448582d34687e80952aff19b487110
pooja-9901 <poojat.xworkz@gmail.com>
2023-07-03 23:22:44 +0530

    classes-and-objects

---
+++ SmartTv.java
@@ -0,0 +1,65 @@
+public class SmartTv{
+	int price;
+	float display;
+	String color;
+	String pictureQuality;
+	int soundQuality;
+	String VoiceControl;
+	String Internetconnectivity;
+	String appStore;
+	String MultiplePorts;
+	String RemoteControl;
+	int energyEfficiency;
+	String compatability;
+	int size;
+	String design;
+	int YearOfRelease;
+	String model;
+	int weight;
+	public static void main(String[] args){
+	
+	SmartTv obj = new SmartTv(20000,21.2f);
+	
+		
+	}
+	
+public SmartTv(int price,float display){
+	this.price=price;
+	this.display=display;
+	System.out.println(price);
+	System.out.println(display);
+	
+}
+
+public static void watching(){
+	
+}
+public static void browsing(){
+	
+}
+public static void gaming(){
+	
+}
+public static void mirroring(){
+	
+}
+public static void downlodingApplications(){
+	
+}
+public static void listeningToMusic(){
+	
+}
+public static void scrollingSocialMedia(){
+	
+}
+public static void voiceAssistance(){
+	
+}
+public static void accessNotification(){
+	
+}
+public static void virtualReality(){
+	
+}
+
+}
